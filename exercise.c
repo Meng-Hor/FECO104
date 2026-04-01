ab2d2f49caeab5bc9393d9d9208f89b4f36ab9d1
Meng-Hor <menghor.l.ing@gmail.com>
2026-04-01 10:34:22 +0700

    update

---
+++ exercise.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    // Exercise 1
+    // int num;
+
+    // printf("Input an integer: ");
+    // scanf("%d", &num);
+
+    // if(num%2==0){
+    //     printf("Even");
+    // }else{
+    //     printf("Odd");
+    // }
+
+    return 0;
+}
