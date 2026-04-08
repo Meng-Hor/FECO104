0bd4e5b9291caef7a583cf70891a1c7a839a0d54
Meng-Hor <menghor.l.ing@gmail.com>
2026-04-08 15:52:00 +0700

    ex19

---
+++ WEEK-01/exercises.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // #1
+    // int n;
+    // cout << "Input an integer: ";
+    // cin >> n;
+    // if(n%2==0){
+    //     cout << "Even";
+    // }else{
+    //     cout << "Odd";
+    // }
+
+    // #2
+    // int num1, num2;
+    // cout << "Input the 1st interger: ";
+    // cin >> num1;
+    // cout << "Input the 2nd interger: ";
+    // cin >> num2;
+    // if(num1>num2){
+    //     cout << "Largest value: " << num1;
+    // }else if(num1<num2){
+    //     cout << "Largest value: " << num2;
+    // }else{
+    //     cout << "The two integer are equal!";
+    // }
+
+    // #3
+    // int i;
+    // for(i=1; i<=10; i++){
+    //     cout << i << endl;
+    // }
+
+    // #4
+    // int n, i=1, sum=0;
+    // while(i<=5){
+    //     cout << "Enter an integer: ";
+    //     cin >> n;
+    //     i++;
+    //     sum = n + sum;
+    // }
+    // cout << "Sum: " << sum;
+
+    // #5
+    // int i, n;
+    // cout << "Enter an integer: ";
+    // cin >> n;
+    // for(i=1; i<=n; i++){
+    //     cout << i << endl;
+    // }
+
+    // #6
+    // int n, max, i;
+    // cout << "Enter an integer: ";
+    // cin >> max;
+    // for(i=1; i<=4; i++){
+    //     cout << "Enter an integer: ";
+    //     cin >> n;
+    //     if(n>max){
+    //         max=n;
+    //     }else{
+    //         max=max;
+    //     }
+    // }
+    // cout << "Maximun value: " << max;
+
+    // #7
+    // int n, min, i;
+    // cout << "Enter an integer: ";
+    // cin >> min;
+    // for(i=1; i<=4; i++){
+    //     cout << "Enter an integer: ";
+    //     cin >> n;
+    //     if(n<min){
+    //         min=n;
+    //     }else{
+    //         min=min;
+    //     }
+    // }
+    // cout << "Minimum value: " << min;
+
+    // #8
+    // int n, i, count=0;
+    // for(i=1;i<=5;i++){
+    //     cout << "Enter an integer: ";
+    //     cin >> n;
+    //     if(n%2==0){
+    //         count++;
+    //     }else{
+    //         count=count;
+    //     }
+    // }
+    // cout << "There are " << count << " even numbers";
+
+    // #9
+    // int n, i, count=0;
+    // for(i=1; i<=5; i++){
+    //     cout << "Enter an integer: ";
+    //     cin >> n;
+    //     if(n>10){
+    //         count++;
+    //     }else{
+    //         count=count;
+    //     }
+    // }
+    // cout << "There are " << count << " numbers greater than 10.";
+
+    // #10
+    // int n, i;
+    // cout << "Enter an integer: ";
+    // cin >> n;
+    // for(i=1; i<=10; i++){
+    //     cout << n << " x " << i << " = " << n*i << endl;
+    // }
+
+    // #11
+    // int n, i, sum=0;
+    // cout << "Enter an integer: ";
+    // cin >> n;
+    // for(i=1; i<=n; i++){
+    //     sum = sum+i;
+    // }
+    // cout << sum;
+
+    // #12
+    // int n, i, count=0;
+    // cout << "Enter an integer: ";
+    // cin >> n;
+    // for(i=1; i<=n; i++){
+    //     if(n%2==0){
+    //         count=count;
+    //     }else{
+    //         count++;
+    //     }
+    //     i++;
+    // }
+    // cout << count;
+
+    // #13
+    // sum=10
+
+    // #14
+    // max=8
+
+    // #15
+    // a: make the first number as the largest value
+    // b: compare the first value to the second value, 
+    // if the second value is bigger, set the second value as the largest value
+    // c: compare the second to the third value and so on.. until the last number
+    // d: print the largest value
+
+    // #16
+    // int i=1;
+    // while(i<=5){
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    // #17
+    // int sum=0;
+    // for(int i=1; i<=5; i++){
+    //     sum+=i;
+    // }
+    // cout << sum;
+    // return 0;
+
+    // #18
+    // int a=5, b=10;
+    // if(a==b){
+    //     cout << "Equal";
+    // }
+
+    // #19
+    // int n, i, a, max, min, sum=0;
+    // cout << "Enter an integer: ";
+    // cin >> a;
+    // min=a;
+    // max=min;
+    // for(i=1; i<=2; i++){
+    //     cout << "Enter an integer: ";
+    //     cin >> n;
+    //     if(n>max){
+    //         max=n;
+    //     }
+    //     if(n<min){
+    //         min=n;
+    //     }
+    //     sum+=n;
+    // }
+    // cout << "Largest value: " << max << endl;
+    // cout << "Smallest value: " << min << endl;
+    // cout << "Sum: " << sum+a << endl;
+
+    return 0;
+}
